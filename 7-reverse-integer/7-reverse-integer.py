9f1ceebc97ddc2ca2cb761b2e7cbcad667f69be5
Karan Praharaj <karanpraharaj@hotmail.com>
2021-09-25 13:20:53 -0600

    Time: 24 ms (97.61%), Space: 4.5 MB (51.54%) - LeetHub

--- 7-reverse-integer/7-reverse-integer.py
+++ 7-reverse-integer/7-reverse-integer.py
@@ -9,11 +9,8 @@ class Solution:
             
         while x1>0:
             r = x1%10
-            print(r)
             x1 = int(x1/10)
-            print(x1)
             sum = (sum*10)+r
-            print(sum)
         
         if flag == 1:
             sum = 0-sum
